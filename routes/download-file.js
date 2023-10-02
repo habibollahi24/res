ad380c0db9e9090bc4d30c48decbd3d99ee0c438
mohammad <habibollahi24@gmail.com>
2023-10-02 22:25:37 +0330

    fifth

---
+++ routes/download-file.js
@@ -0,0 +1,10 @@
+//  Maryam Afshar, Student ID: 301034443, Due Date: October 3, 2023
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.download("./public/pdfs/Resume_Maryam Afshar.pdf");
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,11 @@ router.get("/", (req, res) => {
   res.render("index", { page_name: "home" });
 });
 
-//get pdf file
-router.get("/download-file", (req, res) => {
-  res.download("./public/pdfs/Resume_Maryam Afshar.pdf");
-});
-
 // all nested route
 router.use("/about", require("./about"));
 router.use("/contact", require("./contact"));
 router.use("/services", require("./services"));
 router.use("/products", require("./products"));
+router.use("/download-file", require("./download-file"));
 
 module.exports = router;
